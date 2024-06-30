fd9365030af77a314dbb1bbf2bf0d232ce477422
dipunSoumyadeep1949 <appworkspace1949@gmail.com>
2024-06-30 16:35:14 +0530

    first commit

---
+++ public/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    <Main></Main>
+    </>
+  )
+}
+
+export default App
